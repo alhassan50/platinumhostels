703298131269c9b9b9d1912b783f93f05409cd57
alhassan50 <abdullahialhassanbaaba@example.com>
2023-12-11 17:15:03 +0000

    project setup complete

---
+++ platinumhostels/src/App.js
@@ -0,0 +1,49 @@
+import './styles/App.css';
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+//pages
+import Home from './pages/home';
+import About from './pages/about';
+import Hostel from './pages/hostels';
+import HostelDetail from './pages/hostels/hosteldetail';
+import Team from './pages/team';
+import Gallery from './pages/gallery';
+import Login from './pages/login';
+import BookNow from './pages/booknow';
+
+
+//layouts
+import Main from './shared/layouts/main';
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Main/>}>
+    <Route index element={<Home/>} />
+    <Route path='about' element={<About/>} />
+    <Route path='hostels'>
+      <Route index element={<Hostel/>} />
+      <Route path=':hostelName' element={<HostelDetail/>} />
+    </Route>
+    <Route path='ourteam' element={<Team/>} />
+    <Route path='gallery' element={<Gallery/>} />
+    <Route path='login' element={<Login/>} />
+    <Route path='booknow' element={<BookNow/>} />
+
+    <Route path='*' element={<h1>404</h1>} />
+  </Route>
+))
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
---
+++ platinumhostels/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section className=''>
+      <div className='section-container'>
+        <h1>
+          platinum hostels
+        </h1>
+      </div>
+    </section>
+  )
+}
