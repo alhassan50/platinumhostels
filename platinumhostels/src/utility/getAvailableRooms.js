a2e5cfe2345c93082da1ce3c3f009055c9365fc7
alhassan50 <abdullahialhassanbaaba@example.com>
2024-01-17 15:19:56 +0000

    live testing cors origin bug

--- platinumhostels/src/utility/getAvailableRooms.js
+++ platinumhostels/src/utility/getAvailableRooms.js
@@ -3,20 +3,20 @@ const getAvailableRooms = async (hostelLocation, roomType, gender) => {
   /* const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   await delay(20000); */
   try {
-    /* let responseRAW = await fetch(
+    let responseRAW = await fetch(
       `http://localhost:8888/.netlify/functions/getAvailableRooms?hostelLocation=${hostelLocation}&roomType=${roomType}&gender=${gender}`, {
         headers: {
           'Content-Type': 'application/json'
         }
       }
-    ) */
-    let responseRAW = await fetch(
+    )
+    /* let responseRAW = await fetch(
       `https://platinumfunctions.netlify.app/.netlify/functions/getAvailableRooms?hostelLocation=${hostelLocation}&roomType=${roomType}&gender=${gender}`, {
         headers: {
           'Content-Type': 'application/json',
         },
       }
-    )
+    ) */
 
     if (!responseRAW.ok) {
       throw new Error(`Couldn't fetch rooms! Status: ${responseRAW.status}.`);
