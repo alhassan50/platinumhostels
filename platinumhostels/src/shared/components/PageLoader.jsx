a21a76f407d40a8dab685a94615269b67e4f34d7
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-02-08 18:24:33 +0000

    Modified the page loader component
    The CSS for the loader bar was modified to ensure a smooth animation and
    transition

--- platinumhostels/src/shared/components/PageLoader.jsx
+++ platinumhostels/src/shared/components/PageLoader.jsx
@@ -8,8 +8,10 @@ export default function PageLoader () {
                     <div className={`w-10 h-10 border-primary border-[5px] rounded-b-[50%]`}>
                     </div>
                 </div>
-
-                <div className={`bg-slate-500 w-[10px] h-1 rounded-[50px] page-loader`}>
+                
+                <div className='min-w-full bg-[#eee]'>
+                    <div className={`bg-slate-500 w-[10px] h-1 rounded-[50px] page-loader`}>
+                    </div>
                 </div>
             </div>
         </div>
